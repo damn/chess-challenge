89bf6df8581c7cd8fb292bf6e2e2114140f5e6df
Michael Sappler <msappler@sixreasons.de>
2015-07-24 00:51:36 +0200

    Reduce Memory Requirements

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [grid2d "0.1.0-SNAPSHOT"]]
-  :jvm-opts ["-Xms6G", "-Xmx6G"])
+  :jvm-opts ["-Xms4G", "-Xmx4G"])
